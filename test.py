f35b1c9f8acf30a4db57550a28df7712927a279c
Israel Santana <isantana@inerza.com>
2017-06-28 13:45:26 +0100

    Corrigiendo los cambios para pasar los test

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
-from app import app
-
+"Clase de Test"
 import unittest
 
+from app import app
+
+
 class BasicTestCase(unittest.TestCase):
+    "Clase básica de Test"
 
     def test_index(self):
+        "Comprobando index"
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, b'Hello World!')
+
 
 if __name__ == '__main__':
     unittest.main()
