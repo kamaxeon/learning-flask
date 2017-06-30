01a60e0a0ab1e3b178cf7a56b4250ddee965e742
Israel Santana <isantana@inerza.com>
2017-06-30 14:36:12 +0100

    Test para obtener una tarea que no existe

--- 01_todo_list/test.py
+++ 01_todo_list/test.py
@@ -27,6 +27,16 @@ class BasicTestCase(unittest.TestCase):
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(data['tasks'], [])
 
+    def test_get_a_non_existing_taks(self):
+        """
+        Get a non existing tasks
+        """
+        response = self.tester.get('/todo/api/tasks/99',
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['error'], 'Not Found')
+
 
 if __name__ == '__main__':
     unittest.main()
