ccfdcc2dcdf0fbfa5b074644727f5b1425e0266e
Israel Santana <isantana@inerza.com>
2017-06-30 14:50:00 +0100

    Pasando el test

--- 01_todo_list/app.py
+++ 01_todo_list/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -9,6 +9,18 @@ tasks = []
 @app.route('/todo/api/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [ task for task in tasks if task['id'] ]
+    if not task:
+        abort(404)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
