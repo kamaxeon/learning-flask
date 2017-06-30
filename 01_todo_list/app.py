fe56d9882731fa0c715d1466d4c4bef12d97d58c
Israel Santana <isantana@inerza.com>
2017-06-30 15:37:27 +0100

    Cambios sin finalizar ni funcionar

--- 01_todo_list/app.py
+++ 01_todo_list/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -16,6 +16,34 @@ def get_task(task_id):
     task = [ task for task in tasks if task['id'] ]
     if not task:
         abort(404)
+
+
+@app.route('/todo/api/tasks', methods=['POST'])
+def create_task():
+    if not request.get_json() or not 'title' in request.get_json():
+        abort(400)
+
+    if not tasks:
+        id = 1
+    else:
+        id = tasks[-1]['id'] + 1
+
+    task = {
+        'id': id,
+        'title': request.json['title'],
+        'description': request.json.get('description', ''),
+        'done': False
+    }
+
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/tasks', methods=['DELETE'])
+def delete_tasks():
+    tasks = []
+    return jsonify({'tasks': tasks})
+
 
 @app.errorhandler(404)
 def not_found(error):
--- 01_todo_list/test.py
+++ 01_todo_list/test.py
@@ -17,6 +17,14 @@ class BasicTestCase(unittest.TestCase):
         """
         self.tester = app.test_client(self)
 
+    def tearDown(self):
+        """
+        TearDown
+        """
+        response = self.tester.delete('/todo/api/tasks',
+                                   content_type='application/json')
+
+
     def test_empty_list_taks(self):
         """
         Test empty taks list
@@ -49,6 +57,22 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         #data = json.loads(response.get_data(as_text=True))
         #self.assertEqual(data['error'], 'Not found')
+
+    def test_create_a_valid_new_taks(self):
+        """
+        Create a taks
+        """
+        response = self.tester.post('/todo/api/tasks',
+                                    data=json.dumps(dict(
+                                        title='Title',
+                                        description='Description'
+                                    )),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['task']['title'], 'Title')
+        self.assertEqual(data['task']['description'], 'Description')
+        self.assertEqual(data['task']['done'], False)
 
 
 
