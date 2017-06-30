83e563d96b42b6d352c921799e236862729c472b
Israel Santana <isantana@inerza.com>
2017-06-30 14:30:01 +0100

    Añadiendo lista de tareas

--- 01_todo_list/app.py
+++ 01_todo_list/app.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # coding=utf-8
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+tasks = []
+
+@app.route('/todo/api/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
