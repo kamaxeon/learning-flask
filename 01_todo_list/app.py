1784777db7fca2b98f1c7a15eb53b59882196539
Israel Santana <isra@miscorreos.org>
2017-07-01 12:01:12 +0100

    Añadiendo la función para borrar una tarea

--- 01_todo_list/app.py
+++ 01_todo_list/app.py
@@ -45,9 +45,37 @@ def delete_tasks():
     return jsonify({'tasks': tasks})
 
 
+@app.route('/todo/api/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if not task:
+        abort(404)
+    request_update = request.get_json()
+    if request_update is None:
+        abort(404)
+
+    if 'done' in request_update and type(request_update['done']) is not bool:
+        abort(404)
+
+    task[0]['title'] = request_update.get('title', task[0]['title'])
+    task[0]['description'] = request_update.get('description',
+                                    task[0]['description'])
+    task[0]['done'] = request_update.get('done', task[0]['done'])
+
+    return jsonify({'task': task[0]})
+
+@app.route('/todo/api/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if not task:
+        abort(404)
+    tasks.remove(tasks[0])
+    return ('', 204)
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 
 if __name__ == '__main__':
