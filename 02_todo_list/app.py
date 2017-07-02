70cebfc6975d0d6ebfd42413b1046d2be9aa22c3
Israel Santana <isra@miscorreos.org>
2017-07-02 16:52:23 +0100

    Dejando los test como estaban antes

--- 02_todo_list/app.py
+++ 02_todo_list/app.py
@@ -1,86 +1,88 @@
 #!/usr/bin/env python
 # coding=utf-8
-'Flask todo list api'
+'Flask todo list api using flask-restful'
 from flask import Flask, jsonify, abort, make_response, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)  # pylint: disable=C0103
+api = Api(app)
 
 tasks = []  # pylint: disable=C0103
 
 
-@app.route('/todo/api/tasks', methods=['GET'])
-def get_tasks():
-    'Return the list of tasks'
-    return jsonify({'tasks': tasks})
+class TaskListAPI(Resource):
+    'Task List Api'
+
+    def get(self):
+        'Return the list of tasks'
+        return jsonify({'tasks': tasks})
 
 
-@app.route('/todo/api/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    'Return the task by id'
-    task = [task for task in tasks if task['id'] == task_id]
-    if not task:
-        abort(404)
-    return jsonify({'task': task[0]})
+    def post(self):
+        'Create a new task'
+        if not request.get_json() or 'title' not in request.get_json():
+            abort(400)
+
+        if not tasks:
+            new_id = 1
+        else:
+            new_id = tasks[-1]['id'] + 1
+        task = {
+            'id': new_id,
+            'title': request.get_json()['title'],
+            'description': request.get_json().get('description', ''),
+            'done': False
+        }
+
+        tasks.append(task)
+        return make_response(jsonify({'task': task}), 201)
+
+    def delete(self):
+        'Remote all the tasks'
+        del tasks[:]
+        return jsonify({'tasks': tasks})
 
 
-@app.route('/todo/api/tasks', methods=['POST'])
-def create_task():
-    'Create a new task'
-    if not request.get_json() or 'title' not in request.get_json():
-        abort(400)
+class TaskAPI(Resource):
+    'Task Api'
 
-    if not tasks:
-        new_id = 1
-    else:
-        new_id = tasks[-1]['id'] + 1
-    task = {
-        'id': new_id,
-        'title': request.get_json()['title'],
-        'description': request.get_json().get('description', ''),
-        'done': False
-    }
+    def get(self, task_id):
+        'Return the task by id'
+        task = [task for task in tasks if task['id'] == task_id]
+        if not task:
+            abort(404)
+        return jsonify({'task': task[0]})
 
-    tasks.append(task)
-    return make_response(jsonify({'task': task}), 201)
+    def put(self, task_id):
+        'Update a task'
+        task = [task for task in tasks if task['id'] == task_id]
+        if not task:
+            abort(404)
+        request_update = request.get_json()
+        if request_update is None:
+            abort(404)
 
+        if 'done' in request_update and \
+                not isinstance(request_update['done'], bool):
+            abort(404)
 
-@app.route('/todo/api/tasks', methods=['DELETE'])
-def delete_tasks():
-    'Remote all the tasks'
-    del tasks[:]
-    return jsonify({'tasks': tasks})
+        task[0]['title'] = request_update.get('title', task[0]['title'])
+        task[0]['description'] = request_update.get('description',
+                                                    task[0]['description'])
+        task[0]['done'] = request_update.get('done', task[0]['done'])
 
+        return jsonify({'task': task[0]})
 
-@app.route('/todo/api/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    'Update a task'
-    task = [task for task in tasks if task['id'] == task_id]
-    if not task:
-        abort(404)
-    request_update = request.get_json()
-    if request_update is None:
-        abort(404)
+    def delete(self, task_id):
+        'Delete a task'
+        task = [task for task in tasks if task['id'] == task_id]
+        if not task:
+            abort(404)
+        tasks.remove(tasks[0])
+        return ('', 204)
 
-    if 'done' in request_update and \
-            not isinstance(request_update['done'], bool):
-        abort(404)
-
-    task[0]['title'] = request_update.get('title', task[0]['title'])
-    task[0]['description'] = request_update.get('description',
-                                                task[0]['description'])
-    task[0]['done'] = request_update.get('done', task[0]['done'])
-
-    return jsonify({'task': task[0]})
-
-
-@app.route('/todo/api/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    'Delete a task'
-    task = [task for task in tasks if task['id'] == task_id]
-    if not task:
-        abort(404)
-    tasks.remove(tasks[0])
-    return ('', 204)
+api.add_resource(TaskListAPI, '/todo/api/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/todo/api/tasks/<int:task_id>', endpoint='task')
 
 
 @app.errorhandler(404)
--- 02_todo_list/test.py
+++ 02_todo_list/test.py
@@ -51,8 +51,8 @@ class BasicTestCase(unittest.TestCase):
         response = self.tester.get('/todo/api/tasks/99',
                                    content_type='application/json')
         self.assertEqual(response.status_code, 404)
-        #data = json.loads(response.get_data(as_text=True))
-        #self.assertEqual(data['error'], 'Not found')
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['error'], 'Not found')
 
     def test_create_a_invalid_new_task(self):
         """
@@ -125,8 +125,8 @@ class BasicTestCase(unittest.TestCase):
         response = self.tester.get('/todo/api/tasks/1',
                                    content_type='application/json')
         self.assertEqual(response.status_code, 404)
-        #data = json.loads(response.get_data(as_text=True))
-        #self.assertEqual(data['error'], 'Not found')
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['error'], 'Not found')
 
     def test_get_all_tasks(self):
         """
