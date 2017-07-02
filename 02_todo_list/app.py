621c86d4893772a7ed85b52aaa37e21995932311
Israel Santana <isra@miscorreos.org>
2017-07-02 17:01:42 +0100

    Pasando los test

--- 02_todo_list/app.py
+++ 02_todo_list/app.py
@@ -5,20 +5,20 @@ from flask import Flask, jsonify, abort, make_response, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)  # pylint: disable=C0103
-api = Api(app)
-
+api = Api(app)  # pylint: disable=C0103
 tasks = []  # pylint: disable=C0103
 
 
 class TaskListAPI(Resource):
     'Task List Api'
 
-    def get(self):
+    @staticmethod
+    def get():
         'Return the list of tasks'
         return jsonify({'tasks': tasks})
 
-
-    def post(self):
+    @staticmethod
+    def post():
         'Create a new task'
         if not request.get_json() or 'title' not in request.get_json():
             abort(400)
@@ -37,7 +37,8 @@ class TaskListAPI(Resource):
         tasks.append(task)
         return make_response(jsonify({'task': task}), 201)
 
-    def delete(self):
+    @staticmethod
+    def delete():
         'Remote all the tasks'
         del tasks[:]
         return jsonify({'tasks': tasks})
@@ -46,21 +47,23 @@ class TaskListAPI(Resource):
 class TaskAPI(Resource):
     'Task Api'
 
-    def get(self, task_id):
+    @staticmethod
+    def get(task_id):
         'Return the task by id'
         task = [task for task in tasks if task['id'] == task_id]
         if not task:
-            abort(404)
+            return make_response(jsonify({'error': 'Not found'}), 404)
         return jsonify({'task': task[0]})
 
-    def put(self, task_id):
+    @staticmethod
+    def put(task_id):
         'Update a task'
         task = [task for task in tasks if task['id'] == task_id]
         if not task:
-            abort(404)
+            return make_response(jsonify({'error': 'Not found'}), 404)
         request_update = request.get_json()
         if request_update is None:
-            abort(404)
+            return make_response(jsonify({'error': 'Not found'}), 404)
 
         if 'done' in request_update and \
                 not isinstance(request_update['done'], bool):
@@ -73,7 +76,8 @@ class TaskAPI(Resource):
 
         return jsonify({'task': task[0]})
 
-    def delete(self, task_id):
+    @staticmethod
+    def delete(task_id):
         'Delete a task'
         task = [task for task in tasks if task['id'] == task_id]
         if not task:
@@ -81,14 +85,9 @@ class TaskAPI(Resource):
         tasks.remove(tasks[0])
         return ('', 204)
 
+
 api.add_resource(TaskListAPI, '/todo/api/tasks', endpoint='tasks')
 api.add_resource(TaskAPI, '/todo/api/tasks/<int:task_id>', endpoint='task')
-
-
-@app.errorhandler(404)
-def not_found(error):  # pylint: disable=W0613
-    'Handler for 404'
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
