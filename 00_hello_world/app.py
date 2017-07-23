972eb07ffd0e8a25eb58bcbd3592b90b39ff4d42
Desarrollos PICSL (Casa) <desarrollo@pic-sl.com>
2017-07-23 16:52:50 +0100

    Cambios estéticos, para cumplir con tox

--- 00_hello_world/app.py
+++ 00_hello_world/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
-""" Aplicación inicial que sólo devuelve un hola mundo
-"""
+
+"""Aplicación inicial que sólo devuelve un hola mundo."""
 
 from flask import Flask, jsonify
 
@@ -10,13 +10,13 @@ app = Flask(__name__)  # pylint: disable=C0103
 
 @app.route('/hello_world')
 def hello_world():
-    "Index"
+    """Index."""
     return 'Hello World!'
 
 
 @app.route('/api/hello_world')
 def hello_world_api():
-    "Index json"
+    """Index json."""
     return jsonify({'message': 'Hello World!'})
 
 
