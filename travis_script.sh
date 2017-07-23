aed8587ef76f1932a24519d9b5649d16aadbe343
Desarrollos PICSL (Casa) <desarrollo@pic-sl.com>
2017-07-23 16:57:03 +0100

    Modificado para usar la aplicación 00_hello_world como banco de pruebas

--- travis_script.sh
+++ travis_script.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# For each directory:
+PROYECTS=(00_hello_world 01_todo_list 02_todo_list 03_todo_list 04_todo_list)
+
+# For each proyect:
 #  * remove venv
-#  * create it 
+#  * create it
 #  * install using pip
 #  * and run tox
-for directory in 00_hello_world 01_todo_list 02_todo_list 03_todo_list 04_todo_list
+
+for DIRECTORY in ${PROYECTS[*]}
 do
-  cd $directory
-  rm -fr venv
-  virtualenv -p python3 venv
-  pip install -r requirements.txt
-  source venv/bin/activate
-  tox
-  deactivate
-  cd .. 
+    cd ${DIRECTORY}
+    rm -fr venv
+    virtualenv -p python3 venv
+    if [ ${DIRECTORY} == "00_hello_world" ]; then
+        pip install pip-tools -U
+        pip-compile --output-file requirements-prod.txt requirements-prod.in
+        pip install -r requirements-prod.txt -U
+        pip-compile --output-file requirements-dev.txt requirements-dev.in
+        pip install -r requirements-dev.txt -U
+    else
+        pip install -r requirements.txt
+    fi
+    source venv/bin/activate
+    tox
+    deactivate
+    cd ..
 done
